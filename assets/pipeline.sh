8dbbc7443c378b7f635b5fc58325685e663fa499
sk444 <sk444@lab-rg07-36.cms.waikato.ac.nz>
2021-06-01 16:42:05 +1200

    FinalTestForTypeScriptCommentCheck

--- assets/pipeline.sh
+++ assets/pipeline.sh
@@ -2,19 +2,28 @@ echo $(python3 StaticCheck.py)
 if [ $(python3 StaticCheck.py) = 0 ]; then
 	echo 'config not present'
 	exit 2
-
 fi
 echo 'config check passed'
+
+echo $(python3 StartOfTSFileCheck.py)
+if [ $(python3 StartOfTSFileCheck.py) = 0 ]; then
+	echo 'Test failed. \n You need to place your name and ID at the top of each .ts file.'
+	exit 2
+fi
+echo 'typescript file comment test past'
+
+
 #npm install
 npm run build
 cd src/_test_
 #jest
-#npm run test
+npm run test
 cd ../../../
 
 #can not do the "add ." command as the node module folder causes crashes
 #echo 'about to add .'
 #git add .
+
 echo 'about to add files'
 git add assets/pipeline.sh
 
