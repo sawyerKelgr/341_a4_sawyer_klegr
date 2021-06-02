8482315fac882eb08a8066740b637740b9e02c52
sk444 <sk444@lab-rg19-14.cms.waikato.ac.nz>
2021-06-02 16:37:48 +1200

    q3V1

--- assets/src/__tests__/PastPurchases.test.tsx
+++ assets/src/__tests__/PastPurchases.test.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import BestSellers from './../modules/bestSellers/BestSellers';
+import PastPurchases from './../modules/bestSellers/BestSellers';
 
 import { render } from '@testing-library/react';
 
 it("has correct headding", ()=>{
-    const {getByTestId} = render(<BestSellers></BestSellers>)
+    const {getByTestId} = render(<PastPurchases></PastPurchases>)
     const x = getByTestId('headding');
     expect(x.innerHTML).toEqual("Top 20 best sellers");
 })
